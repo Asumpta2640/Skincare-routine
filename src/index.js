7570a6551c5ff0f7031751f2d63d64403dee2417
Jelagat2640 <sugutasumpta@gmail.com>
2025-06-27 03:42:23 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -84,3 +84,7 @@ generateBtn.addEventListener("click", () => {
 
     tipContainer.innerHTML = `<h3>Tip:</h3><p>${tip}</p>`;
 });
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
